777f9704a92fa1ada2c616d89bd11cc567fc1b1c
vadim.p <vadim.p@gowombat.team>
2024-02-12 00:52:28 +0200

    feat: initial push

---
+++ canvas.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import scrolledtext
+from tkinter.ttk import Combobox
+
+from engine import TemplateGenerator
+
+
+class App(object):
+    window = None
+    engine = None
+    combox = None
+    save_directory = Path.home()
+    templates_directory = Path.home()
+    data_file = None
+    named_header = None
+    save_label = None
+    template_label = None
+
+    def _get_save_directory(self):
+        self.save_directory = filedialog.askdirectory()
+        self.engine.save_folder = self.save_directory
+        self.save_label.config(text=f"Усі сгенеровані документі будуть за шляхом : {self.save_directory}")
+
+    def _get_templates_directory(self):
+        self.templates_directory = filedialog.askdirectory()
+        self.engine.template_folder = self.templates_directory
+        self.template_label.config(text=f"Усі шаблони знаходяться за шляхом : {self.templates_directory}")
+
+    def _get_data_file(self):
+        self.data_file = filedialog.askopenfilename(filetype=(('Excel', '*.xlsx'),
+                                                              ('Excel', '*.xls')))
+        self.engine.data_file = self.data_file
+        self.engine.read_data()
+        try:
+            self.combox['values'] = list(self.engine.excel.columns)
+            self.combox.current(0)
+        except AttributeError:
+            pass
+
+    def _set_buttons(self):
+        select_save_folder = Button((self.window), text='Зберегти в ...', command=(self._get_save_directory), width=20,
+                                    pady=5,
+                                    bd=1)
+        select_load_templates = Button((self.window), text='Папка з шаблонами', command=(self._get_templates_directory),
+                                       width=20,
+                                       pady=5,
+                                       bd=1)
+        select_data_file = Button((self.window), text='Виберіть файл даних', command=(self._get_data_file), width=20,
+                                  pady=5,
+                                  bd=1)
+        submit_button_form = Button((self.window), text='Згенерувати файл', command=(self.submit_from), width=20,
+                                    pady=5,
+                                    bd=1)
+        self.save_label = Label((self.window),
+                                text=f"Усі сгенеровані документі будуть за шляхом : {self.templates_directory}")
+        self.save_label.grid(row=0, column=1, pady=3)
+        self.template_label = Label((self.window),
+                                    text=f"Усі шаблони знаходяться за шляхом : {self.templates_directory}")
+        self.template_label.grid(row=1, column=1, pady=3)
+        self.combox = Combobox()
+        select_save_folder.grid(row=0, column=0, pady=3, padx=3)
+        select_load_templates.grid(row=1, column=0, pady=3, padx=3)
+        select_data_file.grid(row=2, column=0, pady=3, padx=3)
+        self.combox.grid(row=2, column=1, pady=3)
+        submit_button_form.grid(row=3, column=1, pady=15)
+
+    def submit_from(self):
+        self.engine.named_header = self.combox.get()
+        try:
+            self.engine.generate_templates()
+            messagebox.showinfo('Success', 'Усе зроблено вірно')
+        except Exception as e:
+            try:
+                try:
+                    messagebox.showerror('ERROR',
+                                         f"Винкла помилка зробіть скриншот цієї помилки та надішліть розробнику: \n {e}")
+                finally:
+                    e = None
+                    del e
+
+            finally:
+                e = None
+                del e
+
+    def _list_templates(self):
+        txt = scrolledtext.ScrolledText((self.window), width=40, height=10)
+        txt.grid(column=0, row=2)
+
+    def run(self):
+        self.window, self.engine = Tk(), TemplateGenerator()
+        self.window.title('E2W')
+        self.window.geometry('600x200')
+        self._set_buttons()
+        self.window.mainloop()
---
+++ engine.py
@@ -0,0 +1,48 @@
+# uncompyle6 version 3.5.0
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.2 (default, Dec 29 2018, 06:19:36)
+# [GCC 7.3.0]
+# Embedded file name: engine.py
+import datetime as dt, os, uuid, pathlib as pl
+import pandas
+
+from mailmerge import MailMerge
+
+class TemplateGenerator(object):
+
+    def __init__(self, path_to_data=None, path_to_folder=None, where_to_save=None, named_header=None):
+        self.data_file = path_to_data
+        self.template_folder = path_to_folder
+        self.excel = None
+        self.save_folder = where_to_save
+        self.named_header = named_header
+
+    def _serialize_datetimes(self, dict_fields: dict) -> dict:
+        """1970-10-18 00:00:00"""
+        for key, value in dict_fields.items():
+            try:
+                dict_fields[key] = dt.datetime.strptime(value, '%Y-%m-%d %H:%M:%S').strftime('%d.%m.%Y')
+            except:
+                continue
+
+        return dict_fields
+
+    def read_data(self):
+        self.excel = pandas.read_excel(self.data_file).fillna('')
+
+    def generate_templates(self):
+        for idx, row in self.excel.iterrows():
+            write_folder = os.path.join(self.save_folder, str(row[self.named_header]))
+            if os.path.exists(write_folder):
+                write_folder = os.path.join(self.save_folder, str(row[self.named_header]) + str(uuid.uuid4())[:8])
+            os.mkdir(write_folder)
+            for template in self.list_templates():
+                template_docx = MailMerge(template)
+                fields = {variable:str(row[variable]) for variable in template_docx.get_merge_fields()}
+                serialized_fields = self._serialize_datetimes(fields)
+                (template_docx.merge)(**serialized_fields)
+                template_basename = os.path.basename(template)
+                template_docx.write(os.path.join(write_folder, template_basename))
+
+    def list_templates(self):
+        return list(pl.Path(self.template_folder).glob('*.docx'))
